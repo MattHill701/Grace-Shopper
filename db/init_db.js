67f19743f91956bf4569ef427e7ff79889c9abd6
Logan Gibler <Logan.Gibler@gmail.com>
2021-11-15 20:04:41 -0500

    Logans starting branch

--- db/init_db.js
+++ db/init_db.js
@@ -4,28 +4,63 @@ const {
   // other db methods 
 } = require('./index');
 
+
+async function dropTables(){
+
+try {
+  console.log("starting to drop tables")
+
+
+
+
+  console.log("finished dropping tables")
+} catch (error) {
+  throw error
+}
+
+
+}
+
+
 async function buildTables() {
   try {
+    console.log("Starting to build tables")
     client.connect();
+    await client.query(`
+      CREATE TABLE users(
+        id SERIAL PRIMARY KEY,
+        username VARCHAR(255) UNIQUE NOT NULL,
+        password VARCHAR(255) NOT NULL
+      );
 
-    // drop tables in correct order
+      CREATE TABLE products(
+        id SERIAL PRIMARY KEY, 
+        name VARCHAR(255) UNIQUE NOT NULL,
+        description TEXT NOT NULL,
+        price INTEGER,
+        category TEXT NOT NULL
+      );
+    `)
+    
 
     // build tables in correct order
-
+    console.log("finished building tables")
   } catch (error) {
     throw error;
   }
 }
 
-async function populateInitialData() {
+async function rebuildDB() {
   try {
-    // create useful starting data
+    client.connect();
+    await dropTables();
+    await createTables();
   } catch (error) {
     throw error;
   }
 }
 
 buildTables()
-  .then(populateInitialData)
+  .then(rebuildDB)
   .catch(console.error)
   .finally(() => client.end());
