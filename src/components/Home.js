2c50664b76b353a29542ce0b27a9ed5499cf8dd4
Logan Gibler <Logan.Gibler@gmail.com>
2021-11-22 21:29:48 -0500

    navbar operational

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+import { Button } from "react-bootstrap";
+
+const Home = () => {
+
+  return (
+    <div>
+      <h1 className="text-center">Welcome to the Home component</h1>
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/components/Register.js
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+import { Button } from "react-bootstrap";
+
+const Register = () => {
+
+  return (
+    <div>
+      <h1 className="text-center">Welcome to the Register component</h1>
+    </div>
+  );
+};
+
+export default Register;
