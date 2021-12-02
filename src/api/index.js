f767552c22c17f2654ea00995cd04906a6981fff
Logan Gibler <Logan.Gibler@gmail.com>
2021-12-01 20:19:56 -0500

    products showing now with flexbox

--- src/api/index.js
+++ src/api/index.js
@@ -1,17 +1,13 @@
 import axios from "axios";
-const BASE = "https://floating-depths-70534.herokuapp.com/api"
-
+// const BASE = "https://floating-depths-70534.herokuapp.com/api"
+const BASE = "http://localhost:5000/api"
 export async function getAllProducts(){
     try {
         console.log("trying to get all products")
-    const { data } = await axios.get(`${BASE}/products`, {
-        headers: {
-            "Content-Type": "application/json",
-          },
-    })
+    const { data } = await axios.get(`${BASE}/products`)
     
         console.log("this is all products" , data)
-        return data 
+        return data.products
     } catch (error) {
         throw error
     }
