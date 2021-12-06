e0c5f454e22de0ea891e3ca3c2aa87d27f73927c
MattHill701 <85851376+MattHill701@users.noreply.github.com>
2021-12-06 18:43:30 -0500

    Update users.js

--- api/users.js
+++ api/users.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const usersRouter = express.Router();
-// const jwt = require("jsonwebtoken");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
 
 const { getAllUsers, getUserByUsername, createUser } = require("../db/users");
 
@@ -14,42 +15,58 @@ usersRouter.get("/", async (req, res) => {
   });
 });
 
-// usersRouter.post("/register", async (req, res, next) => {
-//   const { username, password } = req.body;
+usersRouter.post("/register", async (req, res, next) => {
+  const { username, password, cart, canSell } = req.body;
 
-//   try {
-//     const _user = await getUserByUsername(username);
+  try {
+    const _user = await getUserByUsername(username);
 
-//     if (_user) {
-//       next({
-//         name: "UserExistsError",
-//         message: "A user by that username already exists",
-//       });
-//     }
+    if (_user) {
+      next({
+        name: "UserExistsError",
+        message: "A user by that username already exists",
+      });
+    }
 
-//     const user = await createUser({
-//       username,
-//       password,
-//     });
+    const user = await createUser({
+      username,
+      password,
+      cart,
+      canSell
+    });
 
-//     const token = jwt.sign(
-//       {
-//         id: user.id,
-//         username,
-//       },
-//       process.env.JWT_SECRET,
-//       {
-//         expiresIn: "1w",
-//       }
-//     );
+    if(!user){
+      next({
+        name: 'UserCreationError',
+        message: 'There was a problem registering you. Please try again.'
+      })
+    }
 
-//     res.send({
-//       message: "thank you for signing up",
-//       token,
-//     });
-//   } catch ({ name, message }) {
-//     next({ name, message });
-//   }
-// });
+    const token = jwt.sign(
+      {
+        id: user.id,
+        username: user.username,
+      },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "1w",
+      }
+    );
+
+    if(!token){
+      next({
+        name: 'TokenCreationError',
+        message: 'There was a problem registering you. Please try again.'
+      })
+    }
+
+    res.send({
+      message: "thank you for signing up",
+      token,
+    });
+  } catch (error) {
+    next(error);
+  }
+});
 
 module.exports = usersRouter;
