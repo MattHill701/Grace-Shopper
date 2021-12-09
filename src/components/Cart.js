726b308735fbf2aaf228cf8c5ffe1cf9818cb62d
Logan Gibler <Logan.Gibler@gmail.com>
2021-12-08 21:01:38 -0500

    mainmain

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,13 +1,26 @@
 import React, { useState } from "react";
 
 
-const Cart = () => {
+const Cart = ({cartItems, setCartItems}) => {
 
   return (
     <div>
       <h1 className="text-center">Shopping Cart</h1>
+      <div className="cart-items">
+           {
+             cartItems.Length ? cartItems.map((item)=>{
+               console.log("this is item",item)
+                return (
+                  <div>
+                    <h3></h3>
+                    <h4></h4>
+                  </div>
+                )
+             }) : null
+           }
+      </div>
     </div>
   );
 };
 
-export default Cart;
+export default Cart
