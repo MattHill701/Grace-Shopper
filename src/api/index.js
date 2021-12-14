329985b0a9120ae86c9b4bf125fc5d1e6b23565e
pink-balloons <ambergray930@yahoo.com>
2021-12-13 20:27:06 -0500

    gitout

--- src/api/index.js
+++ src/api/index.js
@@ -1,29 +1,33 @@
 import axios from "axios";
+import { storeToken, storeUser } from "../auth";
 // const BASE = "https://floating-depths-70534.herokuapp.com/api"
-const BASE = "http://localhost:5000/api"
-import { storeToken, storeUser} from "../auth";
-export async function getAllProducts(){
-    try {
-        // console.log("trying to get all products")
-    const { data } = await axios.get(`${BASE}/products`)
-    
-        // console.log("this is all products" , data)
-        return data.products
-    } catch (error) {
-        throw error
-    }
+const BASE = "http://localhost:5000/api";
+
+export async function getAllProducts() {
+  try {
+    // console.log("trying to get all products")
+    const { data } = await axios.get(`${BASE}/products`);
+
+    // console.log("this is all products" , data)
+    return data.products;
+  } catch (error) {
+    throw error;
+  }
 }
 
 export async function registerUser(username, password, cart, canSell) {
-  console.log("this is whats passed in",username, password, cart, canSell);
+  console.log("this is whats passed in", username, password, cart, canSell);
   try {
-    const { data } = await axios.post(`http://localhost:5000/api/users/register`, {
-      username: username,
-      password: password,
-      cart: cart,
-      canSell: canSell,
-    });
-    console.log("this is register user data",data);
+    const { data } = await axios.post(
+      `http://localhost:5000/api/users/register`,
+      {
+        username: username,
+        password: password,
+        cart: cart,
+        canSell: canSell,
+      }
+    );
+    console.log("this is register user data", data);
     storeToken(data.token);
     //   storeUser(data.user.username);
     return data;
@@ -47,57 +51,65 @@ export async function loginUser(username, password) {
   }
 }
 
-export async function addProductToOrder(add, productId, userId){
+export async function addProductToOrder(add, productId, userId) {
   try {
-    const data = await axios.patch(`http://localhost:5000/api/orders/products`, {
+    const data = await axios.patch(
+      `http://localhost:5000/api/orders/products`,
+      {
         add: add,
         productId: productId,
-        userId: userId
-    })
+        userId: userId,
+      }
+    );
     // console.log("this is productorder data", data)
-    return data
+    return data;
   } catch (error) {
-    throw error
+    throw error;
   }
 }
 
-export async function getOrderById(userId){
+export async function getOrderById(userId) {
   // console.log("this is userId in api", userId)
   // console.log("4")
   try {
-    const { data } = await axios.post(`http://localhost:5000/api/orders/myorder`, {
-      id: userId
-    })
-    console.log("this is all ordersbyId", data)
-    return data
+    const { data } = await axios.post(
+      `http://localhost:5000/api/orders/myorder`,
+      {
+        id: userId,
+      }
+    );
+    console.log("this is all ordersbyId", data);
+    return data;
   } catch (error) {
-    throw error
+    throw error;
   }
 }
 
-export async function finishCart(order){
-//  console.log("this is order", order)
- console.log("this is order.order.products", order.order.products)
+export async function finishCart(order) {
+  //  console.log("this is order", order)
+  console.log("this is order.order.products", order.order.products);
   try {
-
-    const { data } = await axios.post(`http://localhost:5000/api/products/myproducts`,{
-   products: order.order.products
-    })
-    console.log("this is data from finish cart",data)
-    return data
+    const { data } = await axios.post(
+      `http://localhost:5000/api/products/myproducts`,
+      {
+        products: order.order.products,
+      }
+    );
+    console.log("this is data from finish cart", data);
+    return data;
   } catch (error) {
-    throw error
+    throw error;
   }
 }
 
-export async function getAllOrders(){
+export async function getAllOrders() {
   try {
-      // console.log("trying to get all products")
-  const { data } = await axios.get(`${BASE}/orders`)
-  
-      console.log("this is all orders" , data)
-      return data.orders
+    // console.log("trying to get all products")
+    const { data } = await axios.get(`${BASE}/orders`);
+
+    console.log("this is all orders", data);
+    return data.orders;
   } catch (error) {
-      throw error
+    throw error;
   }
 }
