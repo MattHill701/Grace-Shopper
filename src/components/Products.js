197e063f7a8c43708b37f1dd0076e70761a0eb94
Logan Gibler <Logan.Gibler@gmail.com>
2021-12-16 14:20:24 -0500

    picture

--- src/components/Products.js
+++ src/components/Products.js
@@ -2,7 +2,16 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 import { Cart } from "./";
 import "./myStyles.css";
-
+import pic from "./pics/chickenWings.jfif";
+import pic1 from "./pics/sharkFinSoup.jfif";
+import pic2 from "./pics/rackOfLamb.jfif"
+import pic3 from "./pics/petSnackz.jfif";
+import pic4 from "./pics/chickenThighs.jfif";
+import pic5 from "./pics/chickenBreasts.jfif";
+import pic6 from "./pics/porkBacon.jfif";
+import pic7 from "./pics/petSnackz.jfif";
+import pic8 from "./pics/babyBackRibs.jfif";
+import pic9 from "./pics/hotWings.jfif";
 import { Navbar, Nav, Container, Col, Row } from "react-bootstrap";
 import {
   addProductToOrder,
@@ -20,6 +29,8 @@ const Products = ({ allProducts, setAllProducts }) => {
   let history = useHistory()
   // const [order, setOrder] = useState([]);
   // console.log("this is allProducts",allProducts)
+  let arr = [pic, pic1, pic2, pic3, pic4, pic5, pic6, pic7, pic8, pic9]
+  let count = -1
   return (
     <div>
       <Row>
@@ -29,10 +40,14 @@ const Products = ({ allProducts, setAllProducts }) => {
             <div className="products-main-container">
               {allProducts.length
                 ? allProducts.map((product) => {
+
+                  count = count + 1
                     // console.log("this is products", product);
                     return (
                       <div className="products-container" key={`${product.id}`}>
                         <div className="listed-product" key={`${product.id}`}>
+                        <img src={`${arr[count]}`} className="product_pic"></img>
+                      
                           <h3>
                             <Link
                               to={`/products/${product.id}`}
